ccab125d08c7d09ef69e72513be963d1337732aa
2D2021FutureMicrosoft <85426633+2D2021FutureMicrosoft@users.noreply.github.com>
2021-06-20 14:47:06 +0800

    Add files via upload

---
+++ Clear Windows Update History/Clear Windows Update History Windows10 1909 and Older.bat
@@ -0,0 +1,4 @@
+@echo off
+powershell -windowstyle hidden -command "Start-Process cmd -ArgumentList '/s,/c,net stop wuauserv & del %systemroot%\SoftwareDistribution\DataStore\Logs\edb.log & net start wuauserv' -Verb runAs"
+
+
