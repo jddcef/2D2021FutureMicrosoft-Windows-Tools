b61b6f5c74469219bb2f09146a4c8c1b59d998a2
2D2021FutureMicrosoft <85426633+2D2021FutureMicrosoft@users.noreply.github.com>
2021-07-10 10:53:53 +0800

    Update Windows Tweaker.bat

--- Windows Tweaker.bat
+++ Windows Tweaker.bat
@@ -1287,7 +1287,7 @@ reg add "HKLM\Software\Policies\Microsoft\MRT" /v "DontOfferThroughWUAU" /t REG_
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager\Subscriptions" /f
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager\SuggestedApps" /f
 cls
-echo Would you like to exit? (Yes/No)
+echo Windows Optimization Has Been Applied! Would you like to exit? (Yes/No)
 set /p l=
 if "%l%" == "yes" goto :exit
 if "%l%" == "no" goto :exit
