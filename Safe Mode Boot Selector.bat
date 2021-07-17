25c5be32be8a24e3b46e81d615179c72b1586886
2D2021FutureMicrosoft <85426633+2D2021FutureMicrosoft@users.noreply.github.com>
2021-07-17 14:40:22 +0800

    Update Safe Mode Boot Selector.bat

--- Safe Mode Boot Selector.bat
+++ Safe Mode Boot Selector.bat
@@ -4,6 +4,18 @@ mode 50,15
 
 :winxpcheck
 %SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v ProductName | find "Microsoft Windows XP" >nul 2>nul
+%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v ProductName | find "Windows Vista" >nul 2>nul
+%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v ProductName | find "Windows 7" >nul 2>nul
+%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v ProductName | find "Windows 8" >nul 2>nul
+%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v ProductName | find "Windows 8.1" >nul 2>nul
+if %errorlevel% EQU 0 (goto :nosupport) (else :start)
+
+:nosupport
+echo Your Windows Version is not Support This Script.
+echo Supported OS is: Windows 10 Higher OS.
+timeout 2 > nul 
+cls
+goto :exit
 if %errorlevel% EQU 0 (goto :nosupport) else (goto :start)
 
 :start
