22bfec4d64d9154bd16246600308e1e58f5d7c7a
2D2021FutureMicrosoft <85426633+2D2021FutureMicrosoft@users.noreply.github.com>
2021-07-17 14:38:25 +0800

    Update Memory Booster.bat

--- Memory Booster.bat
+++ Memory Booster.bat
@@ -3,6 +3,22 @@ color 03
 title Memory Booster
 cls
 
+:wincheck
+%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v ProductName | find "Microsoft Windows XP" >nul 2>nul
+%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v ProductName | find "Windows Vista" >nul 2>nul
+%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v ProductName | find "Windows 7" >nul 2>nul
+%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v ProductName | find "Windows 8" >nul 2>nul
+%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v ProductName | find "Windows 8.1" >nul 2>nul
+if %errorlevel% EQU 0 (goto :nosupport) (else :start)
+
+:nosupport
+echo Your Windows Version is not Support This Script.
+echo Supported OS is: Windows 10 Higher OS.
+timeout 2 > nul 
+cls
+goto :exit
+
+:start
 echo Welcome To Memory Booster!
 timeout 2 > nul
 cls
