61b18d8c7554b7f3851f5b48d07bd15cd774d4e3
2D2021FutureMicrosoft <85426633+2D2021FutureMicrosoft@users.noreply.github.com>
2021-07-17 14:41:33 +0800

    Update System Restorer.bat

--- System Restorer.bat
+++ System Restorer.bat
@@ -3,9 +3,22 @@ color 03
 title System Restorer
 cls
 
-:SystemRestore
+:wincheck
+%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v ProductName | find "Microsoft Windows XP" >nul 2>nul
+%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v ProductName | find "Windows Vista" >nul 2>nul
+%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v ProductName | find "Windows 7" >nul 2>nul
+%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v ProductName | find "Windows 8" >nul 2>nul
+%SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows NT\CurrentVersion" /v ProductName | find "Windows 8.1" >nul 2>nul
+if %errorlevel% EQU 0 (goto :nosupport) (else :SystemRestore)
 
+:nosupport
+echo Your Windows Version is not Support This Script.
+echo Supported OS is: Windows 10 Higher OS.
+timeout 2 > nul 
 cls
+goto :exit
+
+:SystemRestore
 echo.System Restore
 echo.
 echo.	ID	Option
